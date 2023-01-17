5a0588a7a38d4f15d3d3fcb97b635bc441932088
yasmeendao <dao.ya@northeastern.edu>
2023-01-16 19:14:37 -0500

    pa2 complete

--- pa2/hello.cpp
+++ pa2/hello.cpp
@@ -1,8 +1,12 @@
+//Yasmeen Dao 1/16/2023 
+//Program prints name using symbols
+
 #include <iostream>
 using namespace std;
 
 int main(void)
 {
+// create name
  cout <<"$-- --$   ---   ----> ~--  ---  <---->  -``""> |``   !``   "<<endl;
  cout <<"| |_| |  / _ ^  (  __| |  %_% | | !```  | |``  |  *  % |   "<<endl;
  cout <<"&___  & | |~| | ( |__  |   v  | | ``*|  | ``*| |   * ^ |   "<<endl;
