41e87a86f73b46ce9fefd4993ff5dfc2a1372e88
yasmeendao <dao.ya@northeastern.edu>
2023-03-14 12:30:46 -0400

    update

--- homework4/main.cpp
+++ homework4/main.cpp
@@ -1,5 +1,5 @@
 // Yasmeen Dao
-// 2/13/23 Read words from file and get information
+// last update: 2/14/23 Read words from file and get information
 
 #include <algorithm>
 #include <fstream>
@@ -13,6 +13,7 @@ using namespace std;
 
 // store vowels in global char
 char vowels[10] = {'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'};
+
 // structure to store word information
 struct WordInfo 
 {
@@ -84,7 +85,7 @@ int main() {
   cout << endl;
   // read input from file
   ifstream infile("./" + usertext);
-  // ** have to type "small.txt or large.txt" or else won't open file
+  // Note: ** have to type small.txt or large.txt or else won't open file **
   if (!infile) {
     cout << "can't open the in file" << endl;
   }
@@ -98,26 +99,27 @@ int main() {
   int count = 0;
   while (infile >> word) {
     string lower = word;
+    // add word from file to total word list
     word_list[word]++;
     count++;
     lower[0] = tolower(lower[0]);
+    // find word
     if (word_list.find(word) == word_list.end()) {
       word_list.insert(pair<string, int>(word, 1));
       unique_lower_words.insert(pair<string, int>(lower, 1));
     } else {
-      // word_list[word]++;
       unique_lower_words[lower]++;
-      
     }
   }
 
-  int num_vowels = vowel_counter(word_list);
-  ///////only want to print out words that start with vowel
+  // words that start with vowel and are lower case
+  int num_vowels = vowel_counter(unique_lower_words);
   // declare vector for pairs
   vector<WordInfo> struct_words;
-  map<string, int>::iterator i = word_list.begin();
-  for (i = word_list.begin(); i != word_list.end(); i++) {
-    // structure object
+  map<string, int>::iterator i = unique_lower_words.begin(); 
+  for (i = unique_lower_words.begin(); i != unique_lower_words.end(); i++) 
+  {
+    // create object to store lowercase unique words (case insensitive)
     WordInfo vowel_words;
     string a = i->first;
     int count = i->second;
@@ -130,11 +132,33 @@ int main() {
     struct_words.push_back(vowel_words);
   }
 
+  int total_distinct = vowel_counter(word_list);
+  // only want to print out words that start with vowel and unique
+  // declare vector for pairs
+  vector<WordInfo> t_words;
+  map<string, int>::iterator it = word_list.begin(); 
+  for (it = word_list.begin(); it != word_list.end(); it++) 
+  {
+    // create new object to store total distinct words (case sensitive)
+    WordInfo total_vowel;
+    string at = it->first;
+    int count = it->second;
+    int length = at.length();
+    total_vowel.word = at;
+    total_vowel.count = count;
+    total_vowel.length = length;
+
+    // add words to vector
+    t_words.push_back(total_vowel);
+  }
+
   vector<WordInfo>::iterator iter = struct_words.begin();
   sort(struct_words.begin(), struct_words.end(), sort_count);
+  
   // print out information to outfile
   ofstream outfile("statistics.txt"); // open or overwrite
-  if (!outfile) {
+  if (!outfile) 
+  {
     cout << "can't open the out file" << endl;
     return EXIT_FAILURE;
   }
@@ -162,7 +186,8 @@ int main() {
   cout<<"Enter threshold for distinct words: "<<endl;
   cin>>thresh;
   int num_below_equal = thresh; 
-  int threshold = compare_word_length(struct_words, num_below_equal);
+  // call compare word function
+  int threshold = compare_word_length(t_words, num_below_equal);
   outfile << usertext << " has " << threshold << " distinct words with length "
           << num_below_equal << " or less";
   outfile << endl; 
